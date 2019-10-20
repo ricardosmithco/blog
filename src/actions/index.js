24f2eef95d572fcd867140f5341fb3cacb58d306
redelization <ricardosmithco@gmail.com>
2019-10-19 23:02:22 -0500

    finished post action creator and made dummy post Reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,9 @@
-export const fetchPosts = () =>{
-    return {
-        type: 'FETCH_POSTS'
-    };
-};
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+// how to create asynchronos action creators
+
+export const fetchPosts = () => async (dispatch) => {
+        const response = await jsonPlaceholder.get('/posts');
+
+        dispatch({ type: 'FETCH_POSTS', psyload: response })
+};    
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions/index';
 
-class PostList extends React.Component{
+class PostList extends React.Component{ 
+
+    componentDidMount(){ // executes the instant our component is rendered on screen
+        this.props.fetchPosts() // our action creator is called
+    }
+
     render(){
         return <div>Post List</div>
     }
 }
 
-export default PostList;
+export default connect(null, { fetchPosts: fetchPosts })(PostList);
+// above, we put null because we don't have any mapToProps rn,
+// then our second argument is our action creator: fetchPost
