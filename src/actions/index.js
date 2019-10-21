4456c48fc2f6cf432befa2d1330bd1a30b0fec1e
redelization <ricardosmithco@gmail.com>
2019-10-20 23:58:47 -0500

    made minor changes

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,8 +2,9 @@ import jsonPlaceholder from '../apis/jsonPlaceholder';
 
 // how to create asynchronos action creators
 
-export const fetchPosts = () => async (dispatch) => {
+export const fetchPosts = () => async dispatch => { // could also be => async (dispatch)
+
         const response = await jsonPlaceholder.get('/posts');
 
-        dispatch({ type: 'FETCH_POSTS', psyload: response })
+        dispatch({ type: 'FETCH_POSTS', payload: response })
 };    
